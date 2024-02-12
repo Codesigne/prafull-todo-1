af8026908fe9618a5baf32b5e7aaf6e6fc3ef8a7
Prafull Karanje <prafullkaranje@gmail.com>
2024-02-12 12:41:55 +0530

    Firebase config added

---
+++ src/Firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDqp4CySr2q8v3uIpCZdYcS2IJIct2EKGs",
+    authDomain: "prafull-todo-1.firebaseapp.com",
+    projectId: "prafull-todo-1",
+    storageBucket: "prafull-todo-1.appspot.com",
+    messagingSenderId: "47205080114",
+    appId: "1:47205080114:web:2fbed1e04600cce195b02b",
+    measurementId: "G-YMDKY88MBQ"
+};
+
+// Initialize Firebase
+const firebase_app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(firebase_app);
+
+export default firebase_app
