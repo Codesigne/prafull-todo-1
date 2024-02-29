569cb2d715f3e9deb4e96bab7cb0ee7c3e223632
Prafull Karanje <prafullkaranje@gmail.com>
2024-02-29 14:49:22 +0530

    Static UI done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+import AddTodo from './views/components/AddTodo';
 import TodoList from './views/components/TodoList';
+import Filter from './views/components/filter';
 
 
 function App() {
@@ -8,7 +10,11 @@ function App() {
     <div className="App">
       <main>
         <div className="container">
+          <Filter></Filter>
+          <hr />
           <TodoList> </TodoList>
+          <hr />
+          <AddTodo></AddTodo>
         </div>
       </main>
     </div>
--- src/views/components/TodoListItem/index.js
+++ src/views/components/TodoListItem/index.js
@@ -2,7 +2,7 @@ function TodoListItem(props) {
     return (
         <>
             <li className="list-group-item" >
-                <b>     {props.text}</b>
+                {/* <b>     {props.text}</b> */}
                 <div className="form-check">
                     <input className="form-check-input" type="checkbox" value={props.id} />
                     <label className="form-check-label" htmlFor="flexCheckDefault">
