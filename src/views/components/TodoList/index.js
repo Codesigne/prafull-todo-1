44394ec1d29c5dbcceb4505a174b3162d8e90ec9
Prafull Karanje <prafullkaranje@gmail.com>
2024-02-29 18:59:47 +0530

    adds state to list

--- src/views/components/TodoList/index.js
+++ src/views/components/TodoList/index.js
@@ -1,25 +1,78 @@
+import { useState } from "react"
 import TodoListItem from "../TodoListItem"
 
 const initialState = {
     todo: [
         {
             id: 1,
-            text: 'asdadasdasda'
+            text: 'Sample Task 1',
+            isCompleted: true
 
         }, {
             id: 2,
-            text: 'asdadaswsssdasda'
+            text: 'Sample Task 2',
+            isCompleted: false
 
         }, {
             id: 3,
-            text: 'afghfhsdadasdasda'
+            text: 'Sample Task 3',
+            isCompleted: true
+
+        },
+        {
+            id: 4,
+            text: 'Sample Task 4',
+            isCompleted: false
+
+        }, {
+            id: 5,
+            text: 'Sample Task 5',
+            isCompleted: true
+
+        }, {
+            id: 6,
+            text: 'Sample Task 6',
+            isCompleted: false
 
         },
     ]
 }
+
 function TodoList(props) {
-    let list = initialState.todo.map((listItem) => {
-        return <TodoListItem key={listItem.id} {...listItem} />
+
+    const [stateTodoList, setStateTodoList] = useState(initialState)
+
+    function handleIsCompleted(event) {
+        console.log(event);
+        console.log(event.target.checked);
+        // setStateTodoList(() => {
+        //     stateTodoList.map((item) => {
+        //         if (item.id === event.target.value) {
+        //             return { ...item, isCompleted: event.target.checked }
+
+        //         } else {
+        //             return item
+        //         }
+        //     })
+        // })
+        const updatedList = [...stateTodoList.todo]
+        console.log("updatedList :", updatedList);
+
+        const itemToUpdate = updatedList.find(item => {
+            console.log("item.id :", item.id);
+            console.log("event.target.value :", event.target.value);
+            return item.id === +event.target.value
+        })
+
+        console.log("itemToUpdate :", itemToUpdate);
+
+        itemToUpdate.isCompleted = !itemToUpdate.isCompleted
+        setStateTodoList({ ...stateTodoList, todo: updatedList })
+
+    }
+
+    let list = stateTodoList.todo.map((listItem) => {
+        return <TodoListItem key={listItem.id} handleIsCompleted={handleIsCompleted} {...listItem} />
     })
     return (
         <ul className="list-group">
